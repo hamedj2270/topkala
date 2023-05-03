cbf795cfe73c2f9c824e4110445b118f57b21b6e
hamed jafari <48656572+hamedj2270@users.noreply.github.com>
2023-05-04 03:15:01 +0330

    Update views.py

--- topkala_profile/views.py
+++ topkala_profile/views.py
@@ -4,13 +4,17 @@ from django.shortcuts import render
 # Create your views here.
 
 def Profile(request):
-
+    if request.user.is_authenticated:
+        username = request.user.username
+        email = request.user.email
+        first_name = request.user.first_name
+        last_name = request.user.last_name
+    else:
+        username = None
     context = {
-
-    }
-    return render(request,'profile.html',context)
-
-
-
-def user_panel_menu_component(request : HttpRequest):
-    return render(request,'',context)
+        'user_name': username,
+        'email':email,
+        'first_name':first_name,
+        'last_name':last_name,
+               }
+    return render(request, 'profile.html', context)
