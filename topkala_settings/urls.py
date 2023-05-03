328fc2c143fe8ec79fc21a5b07747de9c056476e
hamed jafari <48656572+hamedj2270@users.noreply.github.com>
2023-05-04 03:09:53 +0330

    Update urls.py

--- topkala_settings/urls.py
+++ topkala_settings/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import admin_page ,admin_page2
+# from .views import admin_page ,admin_page2
 
 urlpatterns = [
-    path('admin_page',admin_page),
-    path('admin_page2',admin_page2),
+#     path('admin_page',admin_page),
+#     path('admin_page2',admin_page2),
 
 ]
