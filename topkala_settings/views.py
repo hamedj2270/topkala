3cda101a5da53df7abe6d38f1d0633715cffe395
hamed jafari <48656572+hamedj2270@users.noreply.github.com>
2023-05-04 03:09:24 +0330

    Update views.py

--- topkala_settings/views.py
+++ topkala_settings/views.py
@@ -3,12 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def admin_page(request):
-    context = {}
+# def admin_page(request):
+#     context = {}
 
-    return render(request, 'index.html', context)
+#     return render(request, 'index.html', context)
 
 
-def admin_page2(request):
-    context = {}
-    return render(request, 'index2.html', context)
+# def admin_page2(request):
+#     context = {}
+#     return render(request, 'index2.html', context)
